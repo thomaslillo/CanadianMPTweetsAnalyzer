671de75347165749ddc4cd58077b831feda002da
thomaslilo <thomas.j.lilo@gmail.com>
2021-08-19 22:15:53 -0400

    updating lambda function

--- lambda_extract_and_load.py
+++ lambda_extract_and_load.py
@@ -1,1 +1,11 @@
 import boto3
+
+def lambda_handler(event, context):
+    client = boto3.resources('dynamodb')
+    table = client.Table('')
+    
+    # where the dict of data to put into the database goes
+    input = {}
+    
+    # add a try and response handeler here
+    response = table.put_item(Item=input)
